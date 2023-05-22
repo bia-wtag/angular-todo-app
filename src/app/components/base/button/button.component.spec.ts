5efefe52a60d9ce6aab06520bc18d8171bf16d4c
Amit Biswas <amit.biswas@welldev.io>
2023-05-22 13:51:06 +0600

    updated: button tests

--- src/app/components/base/button/button.component.spec.ts
+++ src/app/components/base/button/button.component.spec.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
 
 @Component({
   selector: 'app-wrapper-component',
   template: `
-    <app-button>
+    <app-button testId="add-todo-btn">
       <div>{{ createButtonLabel }}</div>
     </app-button>
   `,
@@ -15,26 +15,12 @@ export class WrapperComponent {
   createButtonLabel = '';
 }
 
-describe('ButtonComponent', () => {
+describe('ButtonComponent Emit', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [WrapperComponent, ButtonComponent],
+      declarations: [ButtonComponent],
     });
   });
-
-  it('should display the projected content correctly', () => {
-    const fixture = TestBed.createComponent(WrapperComponent);
-    const wrapper = fixture.componentInstance;
-
-    const projectedElement = fixture.nativeElement.querySelector('button');
-    expect(projectedElement.textContent).toEqual('');
-
-    wrapper.createButtonLabel = 'Create';
-    fixture.detectChanges();
-
-    expect(projectedElement.textContent).toEqual(wrapper.createButtonLabel);
-  });
-
   it('should emit on click', () => {
     const fixture = TestBed.createComponent(ButtonComponent);
     const button = fixture.componentInstance;
@@ -47,5 +33,35 @@ describe('ButtonComponent', () => {
 
     fixture.detectChanges();
     expect(button.clickEvent.emit).toHaveBeenCalled();
+  });
+});
+
+describe('ButtonComponent Integration with Wrapper', () => {
+  let fixture: ComponentFixture<WrapperComponent>;
+  let wrapper: WrapperComponent;
+  let button: HTMLButtonElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [WrapperComponent, ButtonComponent],
+    });
+
+    fixture = TestBed.createComponent(WrapperComponent);
+    wrapper = fixture.componentInstance;
+    button = fixture.nativeElement.querySelector('button');
+    fixture.detectChanges();
+  });
+
+  it('should display the projected content correctly', () => {
+    expect(button.textContent).toEqual('');
+
+    wrapper.createButtonLabel = 'Create';
+    fixture.detectChanges();
+
+    expect(button.textContent).toEqual(wrapper.createButtonLabel);
+  });
+
+  it('should add testId correctly', () => {
+    expect(button.dataset['testId']).toEqual('add-todo-btn');
   });
 });
--- src/app/components/base/button/button.component.ts
+++ src/app/components/base/button/button.component.ts
@@ -1,14 +1,15 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   template: `
-    <button (click)="onClick()">
+    <button [attr.data-test-id]="testId" (click)="onClick()">
       <ng-content></ng-content>
     </button>
   `,
 })
 export class ButtonComponent {
+  @Input() testId = '';
   @Output() clickEvent = new EventEmitter();
 
   onClick() {
